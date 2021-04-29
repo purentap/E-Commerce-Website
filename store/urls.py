6a908a63af61a9e61a7a00a28fb2516c66df03a9
Berfin Keles <berfinkeles@sabanciuniv.edu>
2021-04-29 16:31:43 +0300

    Search feature added

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name = "store"),
+    path('search/', views.search, name = "search"),
     path('cart/', views.cart, name = "cart"),
     path('checkout/', views.checkout, name = "checkout"),
     path('account/', views.account, name = "account"),
