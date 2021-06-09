1f4c23fb1271cbd9a9553eac86c498b49ea1171e
ebektur <BEKTUR@IZTU-D28.iztu.local>
2021-06-09 22:31:59 +0300

    flutter register

--- api/serializers.py
+++ api/serializers.py
@@ -41,6 +41,7 @@ class OrderOnlySerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
--- api/views.py
+++ api/views.py
@@ -40,6 +40,18 @@ class SlSoftDeleteMixin(mixins.DestroyModelMixin):
         instance.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+class UserList(APIView):
+    serializer_class = UserSerializer
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class UserRecordView(APIView):
     """
     API View to create or get a list of all the registered
